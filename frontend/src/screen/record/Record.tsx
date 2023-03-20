3c942e12a4d7989951dd531dca51c5f797ab9f8d
eomji <anipap2@naver.com>
2023-03-20 17:45:05 +0900

    Feat: input 컴포넌트 만드는 중

--- frontend/src/screen/record/Record.tsx
+++ frontend/src/screen/record/Record.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import InputComp from '../../components/common/input/InputComp';
 
 export default function Record(): JSX.Element {
   return (
-    <View>
+    <SafeAreaView>
       <Text>Record</Text>
-    </View>
+      <InputComp />
+    </SafeAreaView>
   );
 }
