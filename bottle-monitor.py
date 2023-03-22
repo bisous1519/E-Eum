f200d1e1354de8a929fdb3e8b10e01912d3e357e
이수련 <dolpong_@naver.com>
2023-03-22 19:47:31 +0900

    UPDATE: 에러 발생 시간과 담당자를 함께 전송할 수 있도록 수정. 전송 로그 추가

--- bottle-monitor.py
+++ bottle-monitor.py
@@ -1,25 +1,61 @@
 import requests
 import subprocess
 import json
+import time
+import datetime
+import pytz
+import logging
+
+# 로그 파일 설정
+logging.basicConfig(filename='bottle.log', level=logging.DEBUG)
 
 # 웹훅 URL
-webhook_url = "https://meeting.ssafy.com/hooks/jn79takmfbbepnodq9ssbhr8iw"
+url = "https://meeting.ssafy.com/hooks/jn79takmfbbepnodq9ssbhr8iw"
 
-# 에러 로그 패턴을 정의합니다. (예시)
+# 에러 로그 패턴을 정의
 error_pattern = "Caused by"
 
 # 로그를 스트리밍하는 커맨드 정의 (Docker 컨테이너를 사용하는 경우)
-container_id = "8428f6810b73"
-stream_logs_cmd = f"docker logs -f {container_id}"
+stream_logs_cmd = f"sudo docker logs bottle"
 
-# 로그를 스트리밍하는 프로세스를 실행합니다.
-process = subprocess.Popen(stream_logs_cmd.split(), stdout=subprocess.PIPE)
+before = 0
 
-# 로그를 실시간으로 읽어 에러 패턴을 찾고, Mattermost에 알림을 전송합니다.
-for line in iter(process.stdout.readline, b''):
-    log_line = line.decode('utf-8')
-    if error_pattern in log_line:
-        payload = {
-            "text": f"**Error detected**:\n```\n{log_line}\n```"
-        }
-        response = requests.post(webhook_url, data=json.dumps(payload), headers={"Content-Type": "application/json"})
+while True:
+    time.sleep(3);
+    process = subprocess.run(stream_logs_cmd, capture_output=True, text=True, shell=True)
+    logs = process.stderr.splitlines()[::-1]
+
+    if len(logs) == before:
+        continue
+    before = len(logs)
+    log_text = ""
+    
+    for log_line in logs:
+        #로그 덧붙이기
+        log_text = log_line + "\n" + log_text
+
+        # 한국 시간 존 이름
+        KST = pytz.timezone('Asia/Seoul')
+
+        # 현재 시간 구하기
+        now = datetime.datetime.now(tz=KST)
+
+        # 년, 월, 일, 시간, 분 출력하기
+        time_str = now.strftime('%Y-%m-%d %H:%M')
+
+        #로그 패턴 발견되면
+        if error_pattern in log_line:
+            #헤더, 데이터 설정
+            headers = {'Content-Type': 'application/json'}
+            data = {'text': f'__Bottle Server__ : @angly97\n```{time_str}```\n```\n{log_text}\n```'}
+
+            curl_command = ['curl', '-i', '-X', 'POST', '-H']
+
+            for k, v in headers.items():
+                #발송
+                curl_command.extend([f"{k}: {v}"])
+                curl_command.extend(['-d', json.dumps(data), url])
+                result = subprocess.run(curl_command, capture_output=True, text=True)
+                logging.debug(f'======{time_str}======')
+                logging.debug(result)
+
