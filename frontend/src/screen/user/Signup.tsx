d0901620aea2b4e4e544a80d02d8ba49e036a589
Hoony-JSG <107928377+Hoony-JSG@users.noreply.github.com>
2023-03-23 14:41:50 +0900

    Feat: 로그인 페이지 구현

--- frontend/src/screen/user/Signup.tsx
+++ frontend/src/screen/user/Signup.tsx
@@ -1,10 +1,22 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useState } from 'react';
+import {
+  Dimensions,
+  Image,
+  Pressable,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import { TextInput } from 'react-native-gesture-handler';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import theme from '../../utils/theme';
+
+const { width: DEVICE_WIDTH, height: DEVICE_HEIGHT } = Dimensions.get('window');
 
 export default function Signup(): JSX.Element {
   return (
     <View>
-      <Text>Signup</Text>
+      <Text>회원가입</Text>
     </View>
   );
 }
