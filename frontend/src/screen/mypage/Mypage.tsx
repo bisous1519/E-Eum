2a92ef1599712d1bfd804371263fcf3908ca1d8c
young <moxnox63@naver.com>
2023-03-23 17:41:13 +0900

    [FE] Feat: 마이프로필 화면 생성

--- frontend/src/screen/mypage/Mypage.tsx
+++ frontend/src/screen/mypage/Mypage.tsx
@@ -1,10 +1,57 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Dimensions, ScrollView, StyleSheet, Text, View } from 'react-native';
+import PlusButton from '../../components/common/PlusButton';
+import theme from '../../utils/theme';
+
+const { width: DEVICE_WIDTH, height: DEVICE_HEIGHT } = Dimensions.get('window');
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'space-around',
+  },
+  profileContainer: {
+    width: DEVICE_WIDTH,
+    height: DEVICE_HEIGHT * 0.3,
+  },
+  profileBox: {
+    flex: 2,
+    margin: 10,
+    width: DEVICE_WIDTH * 0.7,
+    borderColor: theme.mainColor.main,
+    borderWidth: 5,
+    borderTopLeftRadius: 10,
+    borderBottomRightRadius: 10,
+  },
+  badgeContainer: {
+    flex: 3,
+    backgroundColor: theme.mainColor.main,
+    width: DEVICE_WIDTH,
+    borderTopLeftRadius: 50,
+    borderTopRightRadius: 50,
+  },
+});
+
+// 뱃지 목록 임의로 만들게용...
+const badge: string[] = [];
+// ===========================
 
 export default function Mypage(): JSX.Element {
+  const onPressBtn = () => {
+    console.log('나는 수정이 하고 싶다고..!');
+  };
+
   return (
-    <View>
-      <Text>Mypage</Text>
+    <View style={styles.container}>
+      <View style={styles.profileContainer}>
+        <View style={styles.profileBox}>
+          <Text>프로필 이미지</Text>
+        </View>
+      </View>
+      <ScrollView style={styles.badgeContainer}>
+        <Text>뱃지 목록</Text>
+      </ScrollView>
+      <PlusButton onPressPlusBtn={onPressBtn} />
     </View>
   );
 }
--- frontend/src/screen/support/SupportDetail.tsx
+++ frontend/src/screen/support/SupportDetail.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback, useMemo, useRef } from 'react';
 import {
   Image,
   ScrollView,
@@ -20,7 +20,13 @@ import * as Progress from 'react-native-progress';
 import { MaterialIcons } from '@expo/vector-icons';
 // URL Linking
 import * as Linking from 'expo-linking';
+// 후원하기 버튼
 import SupportButton from '../../components/support/SupportButton';
+// 후원금액 입력 모달
+import {
+  BottomSheetModal,
+  BottomSheetModalProvider,
+} from '@gorhom/bottom-sheet';
 
 const { width: DEVICE_WIDTH, height: DEVICE_HEIGHT } = Dimensions.get('window');
 
@@ -29,76 +35,91 @@ export default function SupportDetail(): JSX.Element {
   const link = () => {
     Linking.openURL('https://www.naver.com/');
   };
+  // 후원금액 입력 모달을 띄우기 위해 필요 ======================================
+  const bottomSheetModalRef = useRef<BottomSheetModal>(null);
+  const snapPoints = useMemo(() => ['65%'], []);
+  const handlePresentModalPress = useCallback(() => {
+    bottomSheetModalRef.current?.present();
+  }, []);
+  const handleSheetChanges = useCallback((index: number) => {
+    console.log('handleSheetchanges', index);
+  }, []);
+  // ============================================================================
 
   const onPressSupportBtn = () => {
-    console.log('후원글 작성 API 푸슝');
+    console.log('후원금액을 입력받는 모달이 푸슝~');
   };
 
   return (
-    <ScrollView style={styles.container}>
-      <Image style={styles.picture} source={sample} resizeMode='cover' />
-      <View style={styles.innerContainer}>
-        <Text style={styles.title}>개발자가 되고 싶어요</Text>
-        <View style={styles.group}>
-          <Text style={styles.contentTitle}>후원 요청 내용</Text>
-          <Text style={styles.content}>인강사이트 개발자 강의 수강권</Text>
-          <TouchableOpacity
-            onPress={() => link()}
-            activeOpacity={0.6}
-            style={styles.productLink}
-          >
-            <Text style={styles.linkText}>참고링크</Text>
-          </TouchableOpacity>
-        </View>
-
-        <View style={styles.group}>
-          <Text style={styles.contentTitle}>후원 요청 기간</Text>
-          <Text style={styles.content}>2023.03.20 ~ 2023.04.01</Text>
-        </View>
-
-        <View style={styles.group}>
-          <Text style={styles.contentTitle}>목표 금액</Text>
-          <Text style={styles.content}>110,000원</Text>
-        </View>
-
-        <View style={styles.group}>
-          <Text style={styles.contentTitle}>달성률</Text>
-          <View style={styles.goalBar}>
-            <Progress.Bar
-              progress={60 / 100}
-              width={280}
-              height={10}
-              color={theme.mainColor.main}
-            />
-            <Text>60%</Text>
+    <BottomSheetModalProvider>
+      <ScrollView style={styles.container}>
+        <Image style={styles.picture} source={sample} resizeMode='cover' />
+        <View style={styles.innerContainer}>
+          <Text style={styles.title}>개발자가 되고 싶어요</Text>
+          <View style={styles.group}>
+            <Text style={styles.contentTitle}>후원 요청 내용</Text>
+            <Text style={styles.content}>인강사이트 개발자 강의 수강권</Text>
+            <TouchableOpacity
+              onPress={() => link()}
+              activeOpacity={0.6}
+              style={styles.productLink}
+            >
+              <Text style={styles.linkText}>참고링크</Text>
+            </TouchableOpacity>
           </View>
-        </View>
 
-        <View style={styles.group}>
-          <Text style={styles.contentTitle}>후원자</Text>
-          {/* 후원자 프로필 사진 모아서 보여주기 */}
-        </View>
+          <View style={styles.group}>
+            <Text style={styles.contentTitle}>후원 요청 기간</Text>
+            <Text style={styles.content}>2023.03.20 ~ 2023.04.01</Text>
+          </View>
 
-        {/* 여기는 이제.. 글쓴이 프로필로 가는 버튼 */}
-        <View style={styles.group}>
-          <TouchableOpacity
-            onPress={() => console.log('프로필이 까꿍')}
-            activeOpacity={0.6}
-          >
-            <View style={styles.writerTag}>
-              <Image source={sample} style={styles.writerPicture} />
-              <View>
-                <Text style={styles.writerName}>홍싸피</Text>
-                <Text style={styles.writerIntro}>나는 있잖아요..</Text>
-              </View>
-              <MaterialIcons name='arrow-forward-ios' size={24} color='black' />
+          <View style={styles.group}>
+            <Text style={styles.contentTitle}>목표 금액</Text>
+            <Text style={styles.content}>110,000원</Text>
+          </View>
+
+          <View style={styles.group}>
+            <Text style={styles.contentTitle}>달성률</Text>
+            <View style={styles.goalBar}>
+              <Progress.Bar
+                progress={60 / 100}
+                width={DEVICE_WIDTH * 0.8}
+                height={DEVICE_HEIGHT * 0.025}
+                color={theme.mainColor.main}
+              />
+              <Text>60%</Text>
             </View>
-          </TouchableOpacity>
-        </View>
+          </View>
 
-        <View style={styles.group}>
-          <Text style={styles.mainContent}>
-            {`안녕하세요 저는 누구누군데 이런 꿈을 갖고 있는데 강의 듣고 싶은데 돈은 없는데 열심히 할건데.. 이거 어떻게 들어가는데..??
+          <View style={styles.group}>
+            <Text style={styles.contentTitle}>후원자</Text>
+            {/* 후원자 프로필 사진 모아서 보여주기 */}
+          </View>
+
+          {/* 여기는 이제.. 글쓴이 프로필로 가는 버튼 */}
+          <View style={styles.group}>
+            <TouchableOpacity
+              onPress={() => console.log('프로필이 까꿍')}
+              activeOpacity={0.6}
+            >
+              <View style={styles.writerTag}>
+                <Image source={sample} style={styles.writerPicture} />
+                <View>
+                  <Text style={styles.writerName}>홍싸피</Text>
+                  <Text style={styles.writerIntro}>나는 있잖아요..</Text>
+                </View>
+                <MaterialIcons
+                  name='arrow-forward-ios'
+                  size={24}
+                  color='black'
+                />
+              </View>
+            </TouchableOpacity>
+          </View>
+
+          <View style={styles.group}>
+            <Text style={styles.mainContent}>
+              {`안녕하세요 저는 누구누군데 이런 꿈을 갖고 있는데 강의 듣고 싶은데 돈은 없는데 열심히 할건데.. 이거 어떻게 들어가는데..??
             
             
 줄바꿈 테스트임...
@@ -109,11 +130,23 @@ export default function SupportDetail(): JSX.Element {
 이부분은 스크롤 테스트임...으아아아
 안녕하세요 저는 누구누군데 이런 꿈을 갖고 있는데 강의 듣고 싶은데 돈은 없는데 열심히 할건데.. 이거 어떻게 들어가는데..??
 안녕하세요 저는 누구누군데 이런 꿈을 갖고 있는데 강의 듣고 싶은데 돈은 없는데 열심히 할건데.. 이거 어떻게 들어가는데..??`}
-          </Text>
+            </Text>
+          </View>
         </View>
-      </View>
+      </ScrollView>
       <SupportButton onPressSupportBtn={onPressSupportBtn} />
-    </ScrollView>
+      {/* <SupportButton onPressSupportBtn={handlePresentModalPress} />
+      <BottomSheetModal
+        ref={bottomSheetModalRef}
+        index={1}
+        snapPoints={snapPoints}
+        onChange={handleSheetChanges}
+      >
+        <View>
+          <Text>우와웅🎉</Text>
+        </View>
+      </BottomSheetModal> */}
+    </BottomSheetModalProvider>
   );
 }
 
@@ -126,6 +159,7 @@ const styles = StyleSheet.create({
     marginVertical: 15,
     marginHorizontal: 20,
     fontSize: theme.fontSize.regular,
+    marginBottom: 70,
   },
   group: {
     marginVertical: DEVICE_HEIGHT * 0.01,
@@ -179,6 +213,7 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'space-between',
+    marginTop: 5,
   },
   mainContent: {
     marginHorizontal: 10,
