eb6b4266b012c651f0d086542d6d0cb82e0c58e1
eomji <anipap2@naver.com>
2023-03-27 17:53:32 +0900

    [FE] Feat: 리덕스 세팅하는 중

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -13,11 +13,31 @@ import Nav from './src/components/common/nav/Nav';
 import MainNavigator from './src/navigator/MainNavigator';
 import 'react-native-gesture-handler';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import {
+  focusManager,
+  QueryClient,
+  QueryClientProvider,
+  onlineManager,
+} from '@tanstack/react-query';
+import NetInfo from '@react-native-community/netinfo';
+import { AppState, Platform } from 'react-native';
+import type { AppStateStatus } from 'react-native';
 
 // SplashScreen.preventAutoHideAsync();
+const queryClient = new QueryClient();
 
-const Tab = createBottomTabNavigator();
-const Stack = createNativeStackNavigator();
+// 이렇게 쓰는게 맞는지, 필요한건지 잘 모루겟넹
+// onlineManager.setEventListener((setOnline) => {
+//   return NetInfo.addEventListener((state: any) => {
+//     setOnline(!!state.isConnected);
+//   });
+// });
+
+function onAppStateChange(status: AppStateStatus) {
+  if (Platform.OS !== 'web') {
+    focusManager.setFocused(status === 'active');
+  }
+}
 
 export default function App(): JSX.Element {
   const [isReady, setIsReady] = useState(false);
@@ -41,17 +61,28 @@ export default function App(): JSX.Element {
     loadFonts();
   }, []);
 
+  useEffect(() => {
+    const subscription = AppState.addEventListener('change', onAppStateChange);
+    return () => subscription.remove();
+  }, []);
+
   if (!isReady) return <Landing />;
   else
     return (
-      <ThemeProvider theme={theme}>
-        <SafeAreaView style={{ flex: 1 }}>
-          <StatusBar style='dark' />
-          <NavigationContainer>
-            <MainNavigator />
-            <Nav />
-          </NavigationContainer>
-        </SafeAreaView>
-      </ThemeProvider>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider theme={theme}>
+          <SafeAreaView style={{ flex: 1 }}>
+            <StatusBar style='dark' />
+            <NavigationContainer>
+              <MainNavigator />
+              <Nav />
+            </NavigationContainer>
+          </SafeAreaView>
+        </ThemeProvider>
+      </QueryClientProvider>
     );
 }
+
+// redux: https://133hyun.tistory.com/59
+// react-native-redux:  https://github.com/Yasser-G/react-native-redux
+// AsyncStorage: https://react-native-async-storage.github.io/async-storage/docs/usage
