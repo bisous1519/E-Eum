c5f1ad454d0c217a33972a0e2a1e7858b00c9a19
Hoony-JSG <107928377+Hoony-JSG@users.noreply.github.com>
2023-03-29 13:47:33 +0900

    Feat: 동영상 스와이프 페이지 구현

--- frontend/src/screen/bottle/BottleList.tsx
+++ frontend/src/screen/bottle/BottleList.tsx
@@ -1,45 +1,87 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Text, Pressable } from 'react-native';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 import useDimension from '../../hooks/useDimension';
 import theme from '../../utils/theme';
 // import Video from 'react-native-video';
 import { Video } from 'expo-av';
+import Swiper from 'react-native-swiper';
+import BottleLeft from './BottleLeft';
+import BottleRight from './BottleRight';
 
 const { DEVICE_WIDTH } = useDimension();
 const styles = StyleSheet.create({
+  tempBorderRed: {
+    borderWidth: 1,
+    borderColor: 'red',
+  },
+  tempBorderBlue: {
+    borderWidth: 1,
+    borderColor: 'blue',
+  },
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: theme.mainColor.main,
     // backgroundColor: theme.background,
     // flex: 1,
     // alignItems: 'center',
   },
   backgroundVideo: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    bottom: 0,
-    right: 0,
+    width: '100%',
+    height: '100%',
+    zIndex: 0,
   },
+  popupFromBackground: {
+    position: 'absolute',
+    top: 10,
+    left: 10,
+    zIndex: 1,
+  },
+  leftPagePressableLocation: {
+    position: 'absolute',
+    width: 200,
+    height: 170,
+    borderWidth: 1,
+    borderColor: 'red',
+    top: 290,
+    left: 160,
+  },
+  leftPageRedPressable: {
+    width: 50,
+    height: 50,
+  },
+  leftPageBlackPressable: {},
+  leftPageBluePressable: {},
 });
 
 export default function BottleList(): JSX.Element {
   const beachVideo = require('../../assets/videos/beachblue.mp4');
   return (
-    <View style={styles.container}>
-      <Video
-        source={beachVideo}
-        rate={1.0}
-        // volume={1.0}
-        isMuted={true}
-        // resizeMode='cover'
-        shouldPlay
-        isLooping={true}
-        style={{ width: '100%', height: '100%' }}
-      />
-    </View>
+    <Swiper>
+      <View style={styles.container}>
+        <View style={styles.backgroundVideo}>
+          <BottleLeft />
+        </View>
+        <View style={styles.popupFromBackground}>
+          <View style={styles.leftPagePressableLocation}>
+            <Pressable
+              style={StyleSheet.flatten([
+                styles.tempBorderBlue,
+                styles.leftPageRedPressable,
+              ])}
+            ></Pressable>
+          </View>
+        </View>
+      </View>
+      <View style={styles.container}>
+        <View style={styles.backgroundVideo}>
+          <BottleRight />
+        </View>
+        <View style={styles.popupFromBackground}></View>
+      </View>
+    </Swiper>
     // <KeyboardAwareScrollView
     //   // style={styles.container}
     //   keyboardShouldPersistTaps='handled'
