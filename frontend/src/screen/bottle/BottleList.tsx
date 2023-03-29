4f889df40cdca275dca216ccbeedcb2da93c1f01
Hoony-JSG <107928377+Hoony-JSG@users.noreply.github.com>
2023-03-29 13:11:22 +0900

    Feat: 동영상 배경화면 설정 완료

--- frontend/src/screen/bottle/BottleList.tsx
+++ frontend/src/screen/bottle/BottleList.tsx
@@ -1,61 +1,53 @@
-import React, { useEffect } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
-import InputComp from '../../components/common/input/InputComp';
 import useDimension from '../../hooks/useDimension';
 import theme from '../../utils/theme';
-import { useRecoilState } from 'recoil';
-import { getRecord } from '../../modules/apis/record/recordApis';
-import { recordState } from '../../modules/apis/record/recordAtoms';
-import { RecordStateType } from '../../modules/apis/record/recordAtomTypes';
+// import Video from 'react-native-video';
+import { Video } from 'expo-av';
 
 const { DEVICE_WIDTH } = useDimension();
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: theme.background,
     flex: 1,
+    justifyContent: 'center',
     alignItems: 'center',
+    // backgroundColor: theme.background,
+    // flex: 1,
+    // alignItems: 'center',
+  },
+  backgroundVideo: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
   },
 });
 
 export default function BottleList(): JSX.Element {
-  const [record, setRecord] = useRecoilState<RecordStateType>(recordState);
-
-  const onPressBtn = (): void => {
-    console.log('인증버튼클릭');
-    // console.log(record);
-  };
-
-  const fetchData = async () => {
-    const data = await getRecord(2);
-    setRecord(data);
-  };
-
-  useEffect(() => {
-    fetchData();
-  }, []);
-
+  const beachVideo = require('../../assets/videos/beachblue.mp4');
   return (
-    <KeyboardAwareScrollView
-      // style={styles.container}
-      keyboardShouldPersistTaps='handled'
-      contentContainerStyle={styles.container}
-    >
-      <View style={{ width: DEVICE_WIDTH * 0.8 }}>
-        <Text>BottleList</Text>
+    <View style={styles.container}>
+      <Video
+        source={beachVideo}
+        rate={1.0}
+        // volume={1.0}
+        isMuted={true}
+        // resizeMode='cover'
+        shouldPlay
+        isLooping={true}
+        style={{ width: '100%', height: '100%' }}
+      />
+    </View>
+    // <KeyboardAwareScrollView
+    //   // style={styles.container}
+    //   keyboardShouldPersistTaps='handled'
+    //   contentContainerStyle={styles.container}
+    // >
+    //   <View style={{ width: DEVICE_WIDTH * 0.8 }}>
 
-        <Text>꿈피드</Text>
-        <Text>꿈후원 목록</Text>
-        <Text>{record.tagName}</Text>
-        <Text>{record.content}</Text>
-        <InputComp
-          name='EMAIL'
-          btn={true}
-          btnText='인증'
-          onPressBtn={onPressBtn}
-        />
-      </View>
-    </KeyboardAwareScrollView>
+    //   </View>
+    // </KeyboardAwareScrollView>
   );
 }
-
