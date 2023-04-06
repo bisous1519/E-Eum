98c2ea0360fd40d23ee683053c061ba3854beea1
hiflo <xswaqz@naver.com>
2023-04-07 03:25:07 +0900

    Fix: faq 제안 로직에서 getFile이 아닌 getInputStream으로 데이터셋에 접근하도록 수정

--- backend/user/src/main/java/com/craypas/user/model/service/UserServiceImpl.java
+++ backend/user/src/main/java/com/craypas/user/model/service/UserServiceImpl.java
@@ -3,6 +3,7 @@ package com.craypas.user.model.service;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -381,7 +382,7 @@ public class UserServiceImpl implements UserService {
 		for (int idx = 0; idx < categories.size(); idx++) {
 			try {
 				ClassPathResource resource = new ClassPathResource("keywords/output" + categories.get(idx));
-				BufferedReader br = new BufferedReader(new FileReader(resource.getFile()));
+				BufferedReader br = new BufferedReader(new InputStreamReader(resource.getInputStream()));
 				String line = "";
 				while ((line = br.readLine()) != null) {
 					String[] arr = line.split(pattern);
